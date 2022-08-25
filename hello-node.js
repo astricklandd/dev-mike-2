8fb86ffae0cdeba49a15667953b42ed3c6f0eba3
Strickland <astrickland7@una.edu>
2022-08-25 13:37:06 -0500

    updated express to include Heroku port

--- hello-node.js
+++ hello-node.js
@@ -5,7 +5,10 @@ const app = express()
 console.log("Hello from node");
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Exress')
+  res.send('<h2><red>Hello World from Express</red></h2>')
+  // res.sendFile(path.join(__dirname, 'inde.html')); *THIS IS THE SAME CODE AS SHOWN IN CLASS
+  // FILE PATH UNDEFINED ERROR MESSAGE 
+
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000, () => console.log('server is running...'));
